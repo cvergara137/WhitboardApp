c8a0c971dbbb24a772981fc1fd40cbc535f430a2
Martin Yeh <martinyeh@psu.edu>
2020-11-03 13:05:47 -0500

    Modify pom.xml and nbactions.xml for NetBeans 12 environment

--- src/main/java/com/mycompany/whiteboardv4/App.java
+++ src/main/java/com/mycompany/whiteboardv4/App.java
@@ -20,6 +20,7 @@ public class App {
     
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello Whiteboardv4!");
     }
     
 }
